fdcbdda05774914b5dae442a19b3bde6c631d487
Ritcheldav <ritcheldav@cloudstaff.com>
2015-02-05 14:31:58 +0800

    DAsh

---
+++ CloudstaffTeamManager/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  CloudstaffTeamManager
+//
+//  Created by RitcheldaV on 5/2/15.
+//  Copyright (c) 2015 CLOUDSTAFF. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsTableViewController: UITableViewController {
+
+}
