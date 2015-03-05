a8da3b80a757a02d37a63475f060bd0b726777f9
t0tep <ChristopherCastillo@crk01-wks-D21Mac.local>
2015-03-05 10:41:12 +0800

    Updated

---
+++ CloudstaffTeamManager/SettingsTableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SettingsTableViewController.swift
+//  CloudstaffTeamManager
+//
+//  Created by RitcheldaV on 10/2/15.
+//  Copyright (c) 2015 CLOUDSTAFF. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController, UITextViewDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var autoUpdate: UISwitch!
+    @IBOutlet weak var timeInterval: UITextField!
+    @IBOutlet weak var defaultMessage: UITextView!
+    
+    ///////////////////////  KEYBOARD DISMISS  /////////////////////////
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textViewShouldEndEditing(textView: UITextView) -> Bool {
+        textView.resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        timeInterval.resignFirstResponder()
+        defaultMessage.resignFirstResponder()
+        self.view.endEditing(true)
+    }
+    /////////////////////////////////////////////////////////////////////
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        timeInterval.delegate = self
+        defaultMessage.delegate = self
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 2
+    }
+
+    
+    @IBAction func done(sender: UIBarButtonItem) {
+        performSegueWithIdentifier("bToDashboardDone", sender: self)
+        
+        println("--->>>> \(defaultMessage.text)")
+        
+    }
+  
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        performSegueWithIdentifier("bToDashboardCanceled", sender: self)
+        println("cancel")
+    }
+    
+
+}
